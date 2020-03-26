b936232f3d718ff78e3c36277258d2c00f72c1db
Leyms <leyra017@hotmail.com>
2020-03-26 02:35:19 -0400

    se creo la clase DBContext

---
+++ FinalCtrlMedico/FinalCtrlMedico/Models/ClassContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FinalCtrlMedico.Models
+{
+    public class ClassContext: DbContext
+    {
+        public DbSet<ClassPaciente> Pacientes { get; set; }
+        public DbSet<ClassMedico> Medicos { get; set; }
+        public DbSet<ClassHabitaciones> Habitaciones { get; set; }
+        public DbSet<ClassIngresos> Ingresos { get; set; }
+        public DbSet<ClassCitas> Citas { get; set; }
+        public DbSet<ClassAltas> Altas { get; set; }
+    }
+}
